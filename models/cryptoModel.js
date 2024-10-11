4d292e3dad1bf48323fd385cd8979b8441c679f9
Pranav Kumar Gupta <pranavkumar2998@gmail.com>
2024-10-11 08:41:08 +0530

    define mongoose schema for storing cryptocurrency data

---
+++ models/cryptoModel.js
@@ -0,0 +1,15 @@
+// Defines the structure for storing cryptocurrency data
+
+const mongoose = require("mongoose");
+const { type } = require("os");
+
+const cryptoSchema = new mongoose.Schema({
+  coinId: { type: String, required: true },
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  marketCap: { type: Number, required: true },
+  change24h: { type: Number, required: true },
+  timestamp: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Crypto", cryptoSchema);
