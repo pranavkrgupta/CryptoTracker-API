b981965d7ceafebb750a84ea6c5854240c4e0240
Pranav Kumar Gupta <pranavkumar2998@gmail.com>
2024-10-11 18:01:17 +0530

    add route for the deviation API

--- routes/cryptoRoutes.js
+++ routes/cryptoRoutes.js
@@ -1,10 +1,16 @@
 // defining route to fetch cryptocurrency data via API
 
 const express = require("express");
-const { getCryptoStats } = require("../controllers/cryptoController");
+const {
+  getCryptoStats,
+  getCryptoPriceDeviation,
+} = require("../controllers/cryptoController");
 const router = express.Router();
 
 // Route to get the latest stats of a cryptocurrency
 router.get("/stats", getCryptoStats);
+
+// Route to get the standard deviation of the  price for the last 100 records
+router.get("/deviation", getCryptoPriceDeviation);
 
 module.exports = router;
